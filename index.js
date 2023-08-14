94b80ae377f776bb3e600e761c63fc73eb47c999
RTids <121754534+RTids@users.noreply.github.com>
2023-08-14 02:38:03 +0100

    Added additional info

--- index.js
+++ index.js
@@ -1,40 +1,68 @@
+//Search Movie title after every letter
 document.getElementById('search').addEventListener('keyup', (e) => {
 	let searchName = e.target.value;
 
 	console.log(searchName);
 
-	//Search API for movies
-
+	//API options
 	const url = `http://www.omdbapi.com/?apikey=b2b8b2f0&s=${searchName}`;
-	const posterUrl = `http://img.omdbapi.com/?apikey=b2b8b2f0&s=${searchName}`;
 	const options = {
 		method: 'GET',
 		contentType: 'application/json',
 	};
 
-    document.querySelector('.movie-body').innerHTML = ''
+	//Clear DOM before adding searched movies
+	document.querySelector('.movie-body').innerHTML = '';
 
+	//Fetch request to get some basic info on the Movie
 	fetch(url, options)
 		.then((resp) => resp.json())
 		.then((data) => {
-			console.log(data.Search);
-
 			const results = data.Search;
+
+			//Run the below code on each individual movie that is found
 			const renderResults = results.forEach((movie) => {
-				console.log(movie.Title);
-            
-                document.querySelector('.movie-body').innerHTML += `
+				//2nd Fetch request on Movie using different URL, to get more information from API
+				fetch(
+					`http://www.omdbapi.com/?apikey=b2b8b2f0&t=${movie.Title}`,
+					options
+				)
+					.then((resp) => resp.json())
+					.then((data) => {
+						//Merge both fetch requests into one variable, adding 'watched' and 'rating' variables
+						const mergedMovieData = {
+							title: movie.Title,
+							year: movie.Year,
+							runtime: data.Runtime,
+							plot: data.Plot,
+							poster: movie.Poster,
+							watched: false,
+							rating: 0,
+						};
+
+						//Render Movie to DOM
+						document.querySelector('.movie-body').innerHTML += `
                 <div class='movie'>
                     <div class='movieDetails'>
-                        <h1 class='movieTitle'>${movie.Title}</h1>
-                        <p class='movieYear'>${movie.Year}</p>
+                        <h1 class='movieTitle'>${mergedMovieData.title}</h1>
+                        <p class='movieYear'>${mergedMovieData.year}</p>
+                        <p class='runtime'>${mergedMovieData.runtime}</p>
+                        <p class='plot'>${mergedMovieData.plot}</p>
                     </div>
-                    <img class='moviePoster' src='${movie.Poster}'>
+                    <div class='actions'>
+                    <input class='watched' type='checkbox' id='${mergedMovieData.title}'>
+                    </div>
+                    <img class='moviePoster' src='${mergedMovieData.poster}'>
                 </div>
-                `
+                `;
+					});
 			});
 		})
 		.catch((err) => {
 			console.error('No Movie Found');
 		});
 });
+
+//Bugs:
+//Default poster if one cannot be loaded/found
+//Adding event listener onto dynamically created checkboxes
